fb8c590cb11921b273a480ca8aa457ec640afb66
Asaf Kali <akali93@gmail.com>
2019-12-09 20:11:06 +0000

    Background fix

---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+
+public class ScrollingBackground : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, GameControl.instance.scrollSpeed);
+    }
+
+    private void Update()
+    {
+        if (GameControl.instance.gameOver)
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
