22f7e8be66f095fc40aa22f185d4db073a2df51f
Exploren <33203575+Exploren@users.noreply.github.com>
2019-12-09 19:17:35 +0200

    Sprites

---
+++ Assets/StonePool.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StonePool : MonoBehaviour
+{
+    public int obsticlePoolSize = 5;                                  // How many obsticles to keep on standby.
+    public float spawnDelay = 3f;                                     // How quickly obsticles spawn.
+    public float obsticleMin = -8f;                                   // Minimum x value of the obsticle position.
+    public float obsticleMax = 8f;                                    // Maximum x value of the obsticle position.
+    public float spawnYPosition = -10f;
+    public float yVelocity = 7f;
+    public GameObject original;
+
+    private GameObject[] obsticles;                                   // Collection of pooled obsticles.
+    private int currentColumn = 0;                                    // Index of the current obsticle in the collection.
+    private Vector2 objectPoolPosition = new Vector2(-15, -25);       // A holding position for our unused obsticles offscreen.
+
+    private float timeSinceLastSpawned;
+
+
+    void Start()
+    {
+        timeSinceLastSpawned = 0f;
+
+        // Initialize the obsticles collection.
+        obsticles = new GameObject[obsticlePoolSize];
+        // Loop through the collection... 
+        for (int i = 0; i < obsticlePoolSize; i++)
+        {
+            // ...and create the individual obsticles.
+            obsticles[i] = (GameObject)Instantiate(original, objectPoolPosition, original.transform.rotation, this.transform);
+        }
+    }
+
+
+    // This spawns obsticles as long as the game is not over.
+    void Update()
+    {
+        timeSinceLastSpawned += Time.deltaTime;
+
+        if (GameControl.instance.gameOver || timeSinceLastSpawned < spawnDelay)
+            return;
+
+        Debug.Log("Sending new obsticle");
+        timeSinceLastSpawned = 0f;
+
+        // Set a random y position for the obsticle
+        float spawnXPosition = Random.Range(obsticleMin, obsticleMax);
+
+        GameObject newObsticle = obsticles[currentColumn];
+        // ...then set the current obsticle to that position.
+        newObsticle.transform.position = new Vector2(spawnXPosition, spawnYPosition);
+        Rigidbody2D rb = newObsticle.GetComponent<Rigidbody2D>();
+        rb.velocity = Vector3.up * yVelocity;
+
+        // Increase the value of currentColumn. If the new size is too big, set it back to zero
+        currentColumn++;
+
+        if (currentColumn >= obsticlePoolSize)
+        {
+            currentColumn = 0;
+        }
+    }
+}
